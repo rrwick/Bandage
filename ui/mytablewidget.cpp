ad35d713b0fe5b5f610faedf674d20b08031b1a0
Ryan Wick <rrwick@gmail.com>
2015-07-09 15:17:50 +1000

    Improve MyTableWidget column sizing

--- ui/mytablewidget.cpp
+++ ui/mytablewidget.cpp
@@ -15,8 +15,8 @@ MyTableWidget::MyTableWidget(QWidget * parent) :
 
 void MyTableWidget::resizeEvent(QResizeEvent * event)
 {
-    QTableWidget::resizeEvent(event);
     resizeColumns();
+    QTableWidget::resizeEvent(event);
 }
 
 
@@ -25,29 +25,30 @@ void MyTableWidget::resizeColumns()
     resizeColumnsToContents();
 
     std::vector<int> columnWidths;
-    int totalColumnWidth = 0;
+    int oldTotalColumnWidth = 0;
     for (int i = 0; i < columnCount(); ++i)
     {
         int columnWidth = horizontalHeader()->sectionSize(i);
         columnWidths.push_back(columnWidth);
-        totalColumnWidth += columnWidth;
+        oldTotalColumnWidth += columnWidth;
     }
 
     //If the total column width exceeds the size of the table, just leave things alone.
-    int tableWidth = width() - 2;  //Subtract 2 as a bit of a buffer
-    if (verticalScrollBar()->isVisible())
-        tableWidth -= verticalScrollBar()->width();
-
-    if (totalColumnWidth > tableWidth)
+    int tableWidth = viewport()->width();
+    if (oldTotalColumnWidth > tableWidth)
         return;
 
     //If the code got here, then there is width to spare in the table.  Resize each column to
     //take up the whole width, keeping their relative size.
-    for (size_t i = 0; i < columnWidths.size(); ++i)
+    int newTotalColumnWidth = 0;
+    for (int i = 0; i < columnCount() - 1; ++i)
     {
         int oldColumnWidth = columnWidths[i];
-        double fraction = double(oldColumnWidth) / totalColumnWidth;
+        double fraction = double(oldColumnWidth) / oldTotalColumnWidth;
         int newColumnWidth = fraction * tableWidth;
+        newTotalColumnWidth += newColumnWidth;
         horizontalHeader()->resizeSection(i, newColumnWidth);
     }
+    int lastColumnWidth = tableWidth - newTotalColumnWidth;
+    horizontalHeader()->resizeSection(columnCount() - 1, lastColumnWidth);
 }
