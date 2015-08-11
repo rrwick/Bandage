aee487047ea36a3ce46205a91c4190655f66fb55
Ryan Wick <rrwick@gmail.com>
2015-08-11 13:58:15 +1000

    Working on query paths dialog

--- ui/querypathsdialog.cpp
+++ ui/querypathsdialog.cpp
@@ -2,6 +2,9 @@
 #include "ui_querypathsdialog.h"
 
 #include "../blast/blastquery.h"
+#include "../graph/path.h"
+#include "tablewidgetitemint.h"
+#include "tablewidgetitemdouble.h"
 
 QueryPathsDialog::QueryPathsDialog(QWidget * parent, BlastQuery * query) :
     QDialog(parent),
@@ -9,6 +12,44 @@ QueryPathsDialog::QueryPathsDialog(QWidget * parent, BlastQuery * query) :
 {
     ui->setupUi(this);
 
+
+
+
+    ui->tableWidget->clearContents();
+    ui->tableWidget->setSortingEnabled(false);
+
+    int pathCount = query->getPathCount();
+    ui->tableWidget->setRowCount(pathCount);
+
+    if (pathCount == 0)
+        return;
+
+    QList<Path> paths = query->getPaths();
+
+    for (int i = 0; i < pathCount; ++i)
+    {
+        Path * path = &paths[i];
+
+        QTableWidgetItem * pathString = new QTableWidgetItem(path->getString(true));
+        pathString->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+
+        int length = path->getLength();
+        TableWidgetItemInt * pathLength = new TableWidgetItemInt(formatIntForDisplay(length), length);
+        pathLength->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+
+        double evalueProduct = 0.0; //TEMP
+        TableWidgetItemDouble * pathEvalueProduct = new TableWidgetItemDouble(QString::number(evalueProduct), evalueProduct);
+        pathEvalueProduct->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+
+
+
+        ui->tableWidget->setItem(i, 0, pathString);
+        ui->tableWidget->setItem(i, 1, pathLength);
+        ui->tableWidget->setItem(i, 2, pathEvalueProduct);
+    }
+
+    ui->tableWidget->resizeColumns();
+    ui->tableWidget->setSortingEnabled(true);
 
 
 }
