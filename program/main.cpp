30e4843961c97ed5fe8ebee3ddcbbcc344524b27
Ryan Wick <rrwick@gmail.com>
2015-10-08 15:39:52 +1100

    Update version number

--- program/main.cpp
+++ program/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QApplication::setApplicationName("Bandage");
-    QApplication::setApplicationVersion("0.6.4");
+    QApplication::setApplicationVersion("0.7.0");
 
     QTextStream out(stdout);
     QTextStream err(stderr);
