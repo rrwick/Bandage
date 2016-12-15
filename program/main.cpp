2b5300e8f5f96b5f3cd72e5cdb5ba0c0d870d3d9
Ryan Wick <rrwick@gmail.com>
2016-12-15 23:15:45 +1100

    Increment version

--- program/main.cpp
+++ program/main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
     #endif //Q_OS_WIN32
 
     QApplication::setApplicationName("Bandage");
-    QApplication::setApplicationVersion("0.8.0");
+    QApplication::setApplicationVersion("0.8.1");
 
     QTextStream out(stdout);
     QTextStream err(stderr);
