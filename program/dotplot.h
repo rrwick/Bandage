3778eba3dc57cbd265b6de6c23a18bc60839d2dc
Ivan Sovic <ivan.sovic@gmail.com>
2017-10-16 22:19:12 -0700

    Added dotplot.cpp and dotplot.h to calculate k-mer seed hits. Also, updated the .pro file to compile the new sources.

---
+++ program/dotplot.h
@@ -0,0 +1,42 @@
+/*
+ * dotplot.h
+ *
+ *  Created on: Oct 16, 2017
+ *      Author: Ivan Sovic
+ *      GitHub: @isovic
+ *      Copyright: Ivan Sovic, 2017
+ *      Licence: MIT
+ *
+ * Simple tool that collects all kmer hits between
+ * two sequences. If drawn, this represents a dotplot
+ * between two sequences. Can be used for very simple
+ * mapping as well.
+ */
+
+#ifndef SRC_PROGRAM_DOTPLOT_H_
+#define SRC_PROGRAM_DOTPLOT_H_
+
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+class KmerPos {
+ public:
+  KmerPos() : kmer(0), pos(0) {}
+  KmerPos(int64_t _kmer, int32_t _pos) : kmer(_kmer), pos(_pos) { }
+
+  int64_t kmer;
+  int32_t pos;
+};
+
+struct KmerHit {
+  KmerHit() : x(0), y(0) { }
+  KmerHit(int32_t _x, int32_t _y) : x(_x), y(_y) { }
+
+  int32_t x;
+  int32_t y;
+};
+
+std::vector<KmerHit> findKmerMatches(const std::string& seq1, const std::string& seq2, int32_t k);
+
+#endif
